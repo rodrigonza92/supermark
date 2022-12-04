2f944a46cfb6c23c12f132e4c55e50bb6d1a0823
NoeliaOlea26 <107000980+NoeliaOlea26@users.noreply.github.com>
2022-12-03 22:48:08 -0300

    main de ventana2
    cambiar de acuerdo a la ventana a ejecutar

---
+++ tKinder/main.py
@@ -0,0 +1,15 @@
+
+from tkinter import *
+from ventana2 import *
+
+
+def main():
+    root = Tk()
+    root.wm_title("Crud Python MySQL")
+    app = Ventana(root) 
+    app.mainloop()
+
+
+
+if __name__ == "__main__":
+    main()
